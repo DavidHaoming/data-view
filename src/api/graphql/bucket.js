48b6f66de659fd5096993996ae161613e28d3947
bqx619 <bqx619@gmail.com>
2020-10-15 16:14:58 +0800

    feat: 以 function 的方式组织部分 api 请求时没有 token

--- src/api/graphql/bucket.js
+++ src/api/graphql/bucket.js
@@ -2,7 +2,7 @@ import apolloClient from "@/api/graphql/index"
 import gql from 'graphql-tag'
 
 export let createBucket = function (v) {
-    return apolloClient.mutate({
+    return apolloClient().mutate({
         mutation: gql`mutation ($ownerId: ID!, $newBucket: InputBucket){
             createBucket(ownerId: $ownerId, bucket: $newBucket) {
                 id
--- src/api/graphql/index.js
+++ src/api/graphql/index.js
@@ -2,10 +2,16 @@ import ApolloClient from 'apollo-boost'
 
 import {API_URL} from "@/const"
 import store from '@/store'
-console.log( store.state.token)
-export default new ApolloClient({
-    uri: `${API_URL}/api/graph`, // 配置请求url
-    headers: {
-        Authorization: store.state.token
-    }
-})
+
+const ac = function () {
+    console.log('token', store.state.token)
+    // 以 function 的方式避免实例过早初始化取不到 token
+    return new ApolloClient({
+        uri: `${API_URL}/api/graph`, // 配置请求url
+        headers: {
+            Authorization: store.state.token
+        }
+    })
+}
+
+export default ac
